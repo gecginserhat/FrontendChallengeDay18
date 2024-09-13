8eb3c67a9f0d5bddd8e1f798530065a7489d2687
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-13 22:06:56 +0300

    Add files via upload

---
+++ ES6+ Features/1.Arrow Functions/app.js
@@ -0,0 +1,39 @@
+/*
+const merhaba = function() {
+    console.log("Merhaba");
+}
+merhaba();
+*/
+
+//Arrow function
+/*
+const merhaba = () => {
+    console.log("Merhaba");
+    
+}
+
+merhaba();
+
+*/
+
+/*
+const merhaba = (firstName,lastName) => console.log("Merhaba ",firstName,lastName);
+
+merhaba("Serhat","Geçgin");
+*/
+
+/*
+const cube = function(x) {
+    return x*x*x;
+}
+console.log(cube(4));
+*/
+
+
+//If we are doing a single operation, we can use it like this without using parentheses.
+const cube = x => x*x*x;
+
+console.log(cube(4));
+
+
+
