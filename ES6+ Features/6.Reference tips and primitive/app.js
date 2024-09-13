7ccc452b5c4b17e91b050e865dea5ba72bef2e8d
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-13 22:21:20 +0300

    Add files via upload

---
+++ ES6+ Features/6.Reference tips and primitive/app.js
@@ -0,0 +1,49 @@
+/*
+let a = "Serhat";
+let b = "Geçgin";
+
+if (a === b) {
+    console.log("a ve b eşittir");
+}
+*/
+
+/*
+const person1 = {
+    name: "Serhat",
+    age: 23
+}
+const person2 = {
+    name: "Emirhan",
+    age: 24
+}
+
+if (person1 === person2) {
+    console.log("eşit");
+}
+*/
+
+/*
+const cities = new Map();
+const key = [1,2,3];
+
+cities.set("Ankara",5);
+cities.set("istanbul",15);
+cities.set(key,"Array")
+
+console.log(cities.get(key));
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
